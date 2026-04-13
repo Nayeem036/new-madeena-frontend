ed000bc525c6101b1efa12b07339b7177e9abe41
Nayeem036 <nayeemahmed4441@gmail.com>
2026-04-13 18:32:10 +0530

    Testing webhook

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <footer className="footer">
-      <p>© {new Date().getFullYear()} New Madeena Star Catering. All rights reserved.</p>
+      <p>© {new Date().getFullYear()} New Madeena Star Catering .All rights reserved.</p>
     </footer>
   );
 }
